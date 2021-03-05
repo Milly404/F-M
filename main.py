755b4dc16d376d40378dc00e4ed91981b259d8d7
Milly404 <79937432+Milly404@users.noreply.github.com>
2021-03-05 14:47:11 -0500

    背景
    啥也没干的周五

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pygame
 import random
+import os
 from random import random, randint, seed
 
 WIDTH=800
@@ -11,4 +12,13 @@ HEIGHT=600
 FPS=258
 VEL=10
 
-#images
+#images 照片
+#find the folder of images 找到我们可爱的照片文件夹
+game_folder=os.path.dirname(__file__)
+img_folder=os.path.join(game_folder,"img")
+
+#background 背景
+brackground=pygame.image.load(os.path.join(img_folder,".png"))
+brackground_rect=brackground.get_rect()
+
+#
