b7feb8004cb2278fdaf16099d5f9dee8b53ff6b6
Milly404 <79937432+Milly404@users.noreply.github.com>
2021-03-08 14:59:14 -0500

    cccc

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import random
 import os
 from random import random, randint, seed
 
-WIDTH=800
-HEIGHT=600
+WIDTH=1200
+HEIGHT=900
 FPS=258
 VEL=10
 
@@ -18,7 +18,21 @@ game_folder=os.path.dirname(__file__)
 img_folder=os.path.join(game_folder,"img")
 
 #background 背景
-brackground=pygame.image.load(os.path.join(img_folder,".png"))
+brackground=pygame.image.load(os.path.join(img_folder,"background right.png"))
 brackground_rect=brackground.get_rect()
 
-#
+#setup palyer
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.image.load(os.path.join(img_folder,"WuKong 1.png")).convert()
+
+
+#initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("FM207")
+clock=pygame.time.Clock()
+
+screen.blit(brackground,brackground_rect)
