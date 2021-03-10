78dc171e399d80d79f2febae84e840edaebd44d4
Milly404 <79937432+Milly404@users.noreply.github.com>
2021-03-10 14:52:41 -0500

    Update 207.py

--- 207.py
+++ 207.py
@@ -11,36 +11,43 @@ import controller
 
 pygame.init()
 vInfo = pygame.display.Info()
-# size = WIDTH, HEIGHT = vInfo.current_w, vInfo.current_h #适合大小
-size = WIDTH, HEIGHT = 1200, 900  # 固定大小
+size = WIDTH, HEIGHT = 1200, 900
 FPS = 258
 VEL = 10
 BLACK = 0, 0, 0
 
-# images 照片
-# find the folder of images 找到我们可爱的照片文件夹
+# images
+# find the folder of images
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
 
-# background 背景照片导入
+# background
 background = pygame.image.load(os.path.join(img_folder, "background right.png"))
 background_rect = background.get_rect()
 
-# icon 图标导入
+# icon
 icon = pygame.image.load(os.path.join(img_folder, "icon.png"))
 pygame.display.set_icon(icon)
 
-# player 人物照片导入
+# player
 WuKong1 = pygame.image.load(os.path.join(img_folder, "WuKong 1.png"))
 WuKong1_rect = WuKong1.get_rect()
 
-# initialize pygame and create window 创造窗口
-pygame.init()
-# screen=pygame.display.set_mode(size, pygame.NOFRAME)#无边框
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("FM207")  # give the game a name 给它个名字
+class Player:
 
-# run game 开始冲冲冲
+    def __init__(self, WuKong1):
+        self.x = 139
+        self.y = 602
+
+
+
+# initialize pygame and create window
+pygame.init()
+# screen=pygame.display.set_mode(size, pygame.NOFRAME)# no frame
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("FM207")  # give the game a name
+
+# run game
 while True:
     for event in pygame.event.get():
         # close the window
@@ -51,10 +58,10 @@ while True:
                 sys.exit()
 
 
-    # draw /render打印背景
+    # draw background
     screen.blit(background, background_rect)
 
-    # 打印人物
+    # draw payer
     screen.blit(WuKong1, WuKong1_rect)
 
     pygame.display.update()
