a128a8fa1487e41b5b3639d61b0f37306eb12052
Milly404 <79937432+Milly404@users.noreply.github.com>
2021-03-09 22:42:53 -0500

    收获不大

--- main.py
+++ main.py
@@ -7,55 +7,74 @@ import random
 import os
 from random import random, randint, seed
 
-WIDTH=1200
-HEIGHT=900
+pygame.init()
+vInfo = pygame.display.Info()
+#size = WIDTH, HEIGHT = vInfo.current_w, vInfo.current_h #适合大小
+size = WIDTH, HEIGHT = 1200,900 #固定大小
 FPS=258
 VEL=10
+BLACK = 0,0,0
 
 #images 照片
 #find the folder of images 找到我们可爱的照片文件夹
 game_folder=os.path.dirname(__file__)
 img_folder=os.path.join(game_folder,"img")
 
-#background 背景照片
+#background 背景照片导入
 background=pygame.image.load(os.path.join(img_folder,"background right.png"))
 background_rect=background.get_rect()
 
-#setup palyer 放入人物
-#class Player(pygame.sprite.Sprite):
-    #def __init__(self):
-        #pygame.sprite.Sprite.__init__(self)
-        #self.image=pygame.image.load(os.path.join(img_folder,"WuKong 1.png")).convert()
+#icon 图标导入
+icon = pygame.image.load(os.path.join(img_folder,"icon.png"))
+pygame.display.set_icon(icon)
 
-    #def update(self):
+#player 人物照片导入
+WuKong1=pygame.image.load(os.path.join(img_folder,"WuKong 1.png"))
+WuKong1_rect=WuKong1.get_rect()
 
 
-
-#player 人物照片
-ship=pygame.image.load(os.path.join(img_folder,"WuKong 1.png"))
-ship_rect=ship.get_rect()
-
-#initialize pygame and create window 创造窗户
+#initialize pygame and create window 创造窗口
 pygame.init()
-pygame.mixer.init()
-screen=pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("FM207")
-clock=pygame.time.Clock()
+screen=pygame.display.set_mode(size, pygame.RESIZABLE) #可移动的屏幕有机会再说
+#screen=pygame.display.set_mode(size, pygame.NOFRAME)#无边框
+#screen=pygame.display.set_mode(size, pygame.FULLSCREEN) #诶嘿搞个全屏就快乐了
+pygame.display.set_caption("FM207") #give the game a name 给它个名字
 
-#run game 开始/菜单
+#run game 开始冲冲冲
 while True:
     for event in pygame.event.get():
         #close the window
         if event.type == pygame.QUIT:
-            sys.exit
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE: #close the window with Esc
+                sys.exit()
+
+                # if event.key == pygame.K_UP:
+
+                # elif event.key == pygame.K_DOWN:
+
+        # 测试xy轴
+        #elif event.type == pygame.MOUSEMOTION:#鼠标所在位置
+            #print("[MOUSEMOTION]:", event.pos,event.rel, event.buttons)
+        #elif event.type == pygame.MOUSEBUTTONUP:#鼠标释放
+            #print("[MOUSEBUTTONUP]:", event.pos, event.button)
+        elif event.type == pygame.MOUSEBUTTONDOWN:#鼠标点击
+            print("[MOUSEBUTTONDOWN]:", event.pos, event.button)
+
+        # 可移动并保持移动的窗口
+        #elif event.type == pygame.VIDEORESIZE:
+            #size = WIDTH,HEIGHT = event.size[0],event.size[1]
+            #screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 
     #draw /render打印背景
     screen.blit(background,background_rect)
 
     #打印人物
-    screen.blit(ship, ship_rect)
+    screen.blit(WuKong1, WuKong1_rect)
 
-
+    pygame.display.update()
     pygame.display.flip()
 pygame.quit()
+
 
