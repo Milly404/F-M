1893e2c35e3c828841097fdd4bcd4558e5f5535d
Fiona-03 <62490449+Fiona-03@users.noreply.github.com>
2021-03-23 12:36:18 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,35 +1,98 @@
-import pygame
-import controller
-import os
+#!/usr/bin/python3.4
+# Setup Python ----------------------------------------------- #
+import pygame, sys
+
+# Setup pygame/window ---------------------------------------- #
+mainClock = pygame.time.Clock()
+from pygame.locals import *
 
 pygame.init()
-vInfo = pygame.display.Info()
-size = WIDTH, HEIGHT = 1200,900
-FPS=30
-VEL=10
-y1 = 530
-y2 = 675
-y3 = 830
-BLACK = (0,0,0)
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500), 0, 32)
 
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder,"img")
-pygame.init()
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("FM207")
+font = pygame.font.SysFont(None, 20)
 
-def StartMenu(self, screen, text='Press (UP) To Start'):
-    self.image = pygame.image.load(os.path.join(img_folder, "menu.png"))
-    self.rect = self.image.get_rect()
-    screen.fill()
-    pygame.display.update()
-    self.fill_text(screen)
-    font_size = 24
-    font = pygame.font.SysFont('arial', font_size)
-    font_width, font_height = font.size(str(text))
-    screen.blit(font.render(str(text), True, BLACK), (554, 753))
 
-while True:
-    StartMenu()
-    pygame.display.update()
-pygame.quit()
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
+click = False
+
+
+def main_menu():
+    while True:
+
+        screen.fill((0, 0, 0))
+        draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+
+        mx, my = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+def game():
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        draw_text('game', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+def options():
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        draw_text('options', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+main_menu()
