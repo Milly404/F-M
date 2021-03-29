da5dfda10ff5fa894dbaee019f63a85713eb4796
Milly404 <79937432+Milly404@users.noreply.github.com>
2021-03-29 13:20:33 -0400

    在正式版添加了障碍和菜单

--- 207.py
+++ 207.py
@@ -16,6 +16,9 @@ vInfo = pygame.display.Info()
 size = WIDTH, HEIGHT = 1200, 900
 FPS = 30
 VEL = 10
+game_over = True
+running = True
+font = pygame.font.SysFont('Arial', 45)
 
 # images
 # find the folder of images
@@ -26,18 +29,18 @@ img_folder = os.path.join(game_folder, "img")
 menu = pygame.image.load(os.path.join(img_folder,"menu.png"))
 menu_rect = menu.get_rect()
 
-# background
+# background images
 bg1 = pygame.image.load(os.path.join(img_folder,"background left.png"))
 bg2 = pygame.image.load(os.path.join(img_folder,"background left.png"))
 x1 = 0
 x2 = -1200
 
-# icon
+# icon images
 icon = pygame.image.load(os.path.join(img_folder, "icon.png"))
 pygame.display.set_icon(icon)
 
-# player
-#run images
+# player images
+# run images
 player_run = []
 player_run.append(pygame.image.load('img\WuKong 1.png'))
 player_run.append(pygame.image.load('img\WuKong 2.png'))
@@ -45,13 +48,39 @@ player_run.append(pygame.image.load('img\WuKong 3.png'))
 player_run.append(pygame.image.load('img\WuKong 4.png'))
 player_run.reverse()
 
-#jump images
+# jump images
 player_jump = []
 player_jump.append(pygame.image.load('img\WuKong 6.png'))
 player_jump.append(pygame.image.load('img\WuKong 5.png'))
 player_jump.append(pygame.image.load('img\WuKong 6.png'))
 
-def bg_move():
+# obstacle images
+obstacle_img = []
+obstacle_img.append(pygame.image.load('img\Stone.PNG'))
+obstacle_img.append(pygame.image.load('img\HuLu.PNG'))
+obstacle_img.append(pygame.image.load('img\JingJiaoDaWang.PNG'))
+obstacle_img.append(pygame.image.load('img\PanTao.PNG'))
+
+def draw_text(text, font, color, surface, x, y): #show the text
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+def show_go_screen(): #start menu
+    screen.blit(menu, menu_rect)
+    draw_text('Press  (A)  to  start', font, (45, 95, 204), screen, 420, 658)
+    pygame.display.flip()
+    waiting = True
+    while waiting:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                waiting = False
+
+def bg_move(): #bargound moving
 
     global x1,x2
 
@@ -69,27 +98,29 @@ def bg_move():
     return x1, x2
 
 #setup player
-class Player(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite): #add the player
 
    def __init__(self):
        pygame.sprite.Sprite.__init__(self)
 
        super().__init__()
-       #self.run_animation = False
        self.animation_speed = 8
        self.animation_counter = 0
-       self.frame_ratio = int(FPS/self.animation_speed)
+       self.frame_ratio = int(FPS / self.animation_speed)
        self.sprites = player_run
        self.current_sprite = 0
        self.image = self.sprites[self.current_sprite]
        self.rect = self.image.get_rect()
+       self.radius = int(self.rect.width / 2.4)
 
-       self.image=pygame.image.load(os.path.join(img_folder,"WuKong 1.png"))
-       self.rect=self.image.get_rect()
-       self.level=2
+       self.rect.center = 139, 602
+       self.y_speed = 1
+       self.rect.bottom = 830
+       self.level = 2
+       self.levelChange = 10
        self.joystick_pressed = False
 
-   def run_animation(self,speed):
+   def run_animation(self,speed): # make the picture change like play are running
 
        global FPS
 
@@ -131,9 +162,23 @@ class Player(pygame.sprite.Sprite):
        else:
            self.rect.bottom = y3
 
-all_sprites=pygame.sprite.Group() #group all of them
-player=Player()
-all_sprites.add(player) #add player1
+class obstacle(pygame.sprite.Sprite): #add the obstacle
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = obstacle_img
+        self.current_sprite = randint(0,3)
+        self.image = self.sprites[self.current_sprite]
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width / 2.5)
+        y1=randint(0,2)*150+560 # made them randint in three level
+        self.rect.bottomleft = (randint(970,1000000),y1)
+        self.start_time=pygame.time.get_ticks()
+
+    def update(self):
+        if self.start_time and pygame.time.get_ticks()-self.start_time>2000:
+            self.start_time=False
+        self.rect.x-=5 #moving to the laft by 5 speeds
 
 # initialize pygame and create window
 pygame.init()
@@ -144,6 +189,19 @@ pygame.display.set_caption("FM207")  # give the game a name
 
 # run game
 while True:
+
+    if game_over:
+        show_go_screen()
+        game_over = False
+        all_sprites = pygame.sprite.Group()  # group all of them
+        obstacles = pygame.sprite.Group()
+        for Obstacle in range(1, 10000):
+            Obstacle = obstacle()
+            all_sprites.add(Obstacle)  # add obstacle
+            obstacles.add(Obstacle)
+        player = Player()
+        all_sprites.add(player)  # add player1
+
     clock.tick(FPS)
 
     p1.update()
@@ -160,6 +218,13 @@ while True:
 
     # draw payer
     all_sprites.update()
+
+
+    #check to see if hit
+    hits = pygame.sprite.spritecollide(player, obstacles, False, pygame.sprite.collide_circle)
+    if hits:
+        game_over = True
+
     all_sprites.draw(screen)
 
     pygame.display.update()
